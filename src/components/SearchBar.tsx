7d0885739831a5276c4063e2154a2d07896a17c8
Enzorbt <enzo.robert@supinfo.com>
2024-06-04 10:58:11 +0200

    :sparkles: Search bar fixed

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -11,20 +11,25 @@ const SearchBar: React.FC = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="flex items-center mt-5">
-            <input
-                type="text"
-                value={query}
-                onChange={(event) => setQuery(event.target.value)}
-                className="border border-gray-500 bg-transparent rounded-full px-4 py-2 w-full text-black"
-                placeholder="Search..."
-            />
-            <button
-                type="submit"
-                className="bg-transparent text-gray-500 opacity-70 rounded-full p-2 flex items-center justify-center"
-            >Search
-            </button>
-        </form>
+        <>
+            <div className="flex items-center justify-center">
+                <form onSubmit={handleSubmit}
+                      className="flex items-center mt-5 border border-gray-500 bg-transparent rounded-full w-4/5">
+                    <input
+                        type="text"
+                        value={query}
+                        onChange={(event) => setQuery(event.target.value)}
+                        className="bg-transparent rounded-full px-4 py-2 w-full text-black"
+                        placeholder="Search..."
+                    />
+                    <button
+                        type="submit"
+                        className="bg-transparent text-gray-500 opacity-70 rounded-full p-2 flex items-center justify-center border-2"
+                    >Search
+                    </button>
+                </form>
+            </div>
+        </>
     );
 };
 
